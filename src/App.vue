2aa6513dd31f817a6ecc5e6f802ccb466e051ee7
Edna Dande <eamondi@jkavitamac.local>
2023-06-12 23:07:24 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+// sr/App.vue
+<script>
+export default {
+  data() {
+    return {
+      greeting: "Hello, World!",
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="message">{{ greeting }}</div>
+</template>
+
+<style lang="scss">
+@import "./styles.scss";
+</style>
